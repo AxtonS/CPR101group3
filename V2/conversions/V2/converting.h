762b012570748db01ca39e1b888a916f027f5c43
Axton Smith <axtonsmith@gmail.com>
2023-12-08 18:02:18 -0500

    Added main and conversions directories to V2

---
+++ V2/conversions/V2/converting.h
@@ -0,0 +1,11 @@
+#ifndef _CONVERTING_H_
+#define _CONVERTING_H_
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+// User inputs a number as a string, then it is returned as an int
+void converting(void);
+
+#endif
---
+++ V2/conversions/V2/main.c
@@ -0,0 +1,7 @@
+#include "converting.h"
+
+int main(void)
+{
+	converting();
+	return 0;
+}
