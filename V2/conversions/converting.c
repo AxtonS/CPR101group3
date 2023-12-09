a43fbc4a9b1fb232cd9c5c46d4bbff26133c5afb
Axton Smith <axtonsmith@gmail.com>
2023-12-08 22:05:42 -0500

    Add conversions module to V2

--- V2/conversions/converting.c
+++ V2/conversions/converting.c
@@ -24,8 +24,21 @@ void converting() {
 	printf("*** End of Converting Strings to int Demo ***\n\n");
 
 /* Version 2 */
-//>> insert here
-
+	printf("*** Start of Converting Strings to double Demo ***\n");
+	char doubleString[BUFFER_SIZE];
+	double doubleNumber;
+	do
+	{
+		printf("Type the double numeric string (q - to quit):\n");
+		fgets(doubleString, BUFFER_SIZE, stdin);
+		doubleString[strlen(doubleString) - 1] = '\0';
+		if ((strcmp(doubleString, "q") != 0))
+		{
+			doubleNumber = atof(doubleString);
+			printf("Converted number is %f\n", doubleNumber);
+		}
+	} while (strcmp(doubleString, "q") != 0);
+	printf("*** End of Converting Strings to double Demo ***\n\n");
 
 /* Version 3 */
 //>> insert here
